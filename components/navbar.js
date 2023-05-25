216409aacf56c8d9f15667d3621011ead57da546
Asia4046 <kirito234875@gmail.com>
2023-05-25 19:25:47 +0530

    main

--- components/navbar.js
+++ components/navbar.js
@@ -31,7 +31,7 @@ function Navbar(props) {
     <header className={`${props.className}`}>
       <div className="flex h-16 w-auto items-center justify-between px-5 lg:mx-auto lg:w-[80%] lg:px-0 text-[#dbdcdd]">
         <div className="pb-2 font-outfit text-4xl font-semibold lg:block text-white">
-          <Link href="/">moopa</Link>
+          <Link href="/">Ani-Felx</Link>
         </div>
 
         {/* Mobile Hamburger */}
